dd7bbe866644a5a4b7f0fbfc9b2a1ae75a939c8e
Naveed <raysnaveed02@gmail.com>
2026-02-17 13:28:58 +0700

    Create Employee Entity

---
+++ src/main/java/com/istad/employee_system/model/Employee.java
@@ -0,0 +1,23 @@
+package com.istad.employee_system.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+}
