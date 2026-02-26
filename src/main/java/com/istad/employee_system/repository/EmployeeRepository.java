a2c2cc0c52143c14070abfe25150203e4cbd6dc4
Naveed <raysnaveed02@gmail.com>
2026-02-26 09:59:39 +0700

    Add search and pagination method in EmployeeRepository

--- src/main/java/com/istad/employee_system/repository/EmployeeRepository.java
+++ src/main/java/com/istad/employee_system/repository/EmployeeRepository.java
@@ -1,11 +1,19 @@
 package com.istad.employee_system.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.istad.employee_system.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // سارے بنیادی فنکشنز (Save, Delete, Find) اس میں پہلے سے موجود ہیں۔
-    // ہمیں کچھ لکھنے کی ضرورت نہیں!
+
+    // یہ کیوری (Query) ایمپلائی کو First Name یا Last Name کے ذریعے سرچ کرے گی
+    // اور ساتھ ہی اسے پیجز (Pagination) میں بھی بانٹ دے گی
+    @Query("SELECT e FROM Employee e WHERE LOWER(e.firstName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(e.lastName) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<Employee> searchEmployees(@Param("keyword") String keyword, Pageable pageable);
+
 }
