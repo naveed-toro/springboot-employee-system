b859c42a14d0aefdb5bbf145c0338754490e2712
Naveed <raysnaveed02@gmail.com>
2026-02-26 16:02:01 +0700

    Create DepartmentDto to format response data

---
+++ src/main/java/com/istad/employee_system/dto/DepartmentDto.java
@@ -0,0 +1,46 @@
+package com.istad.employee_system.dto;
+
+public class DepartmentDto {
+
+    private Long id;
+    
+    private String departmentName;
+    
+    private String departmentDescription;
+
+    // خالی کنسٹرکٹر
+    public DepartmentDto() {
+    }
+
+    // پیرامیٹر والا کنسٹرکٹر
+    public DepartmentDto(Long id, String departmentName, String departmentDescription) {
+        this.id = id;
+        this.departmentName = departmentName;
+        this.departmentDescription = departmentDescription;
+    }
+
+    // --- Getters and Setters ---
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentDescription() {
+        return departmentDescription;
+    }
+
+    public void setDepartmentDescription(String departmentDescription) {
+        this.departmentDescription = departmentDescription;
+    }
+}
